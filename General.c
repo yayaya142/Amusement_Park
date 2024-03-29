396c11320449bc8d8245f3ac5dc2276b64a42ebc
shai <yayaya142@gmail.com>
2024-03-29 16:02:56 +0300

    add general libaries

---
+++ General.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "General.h"
+
+
+
+char* getStrExactName(const char* msg)
+{
+	char* str;
+	char temp[MAX_STR_LEN];
+	printf("%s\t", msg);
+	myGets(temp, MAX_STR_LEN);
+
+	str = getDynStr(temp);
+	return str;
+}
+
+char* getDynStr(char* str)
+{
+	char* theStr;
+	theStr = (char*)malloc((strlen(str) + 1) * sizeof(char));
+	if (!theStr)
+		return NULL;
+
+	strcpy(theStr, str);
+	return theStr;
+}
+
+char* myGets(char* buffer, int size)
+{
+	char* ok;
+	if (buffer != NULL && size > 0)
+	{
+		do { //skip only '\n' strings
+			ok = fgets(buffer, size, stdin);
+		} while (ok && ((strlen(buffer) <= 1) && (isspace(buffer[0]))));
+		if (ok)
+		{
+			char* back = buffer + strlen(buffer);
+			//trim end spaces
+			while ((buffer < back) && (isspace(*--back)));
+			*(back + 1) = '\0';
+			return buffer;
+		}
+		buffer[0] = '\0';
+	}
+	return NULL;
+}
+
+char** splitCharsToWords(char* str, int* pCount, int* pTotalLength)
+{
+	char temp[255];
+	char* delimiters = " ";
+	char* word;
+	int count = 0;
+
+	strcpy(temp, str);
+	char** wordsArray = NULL;
+	*pTotalLength = 0;
+
+	word = strtok(temp, delimiters);
+	while (word != NULL)
+	{
+		wordsArray = (char**)realloc(wordsArray, (count + 1) * sizeof(char*));
+		if (!wordsArray)
+			return 0;
+		wordsArray[count] = getDynStr(word);
+		count++;
+		*pTotalLength += (int)strlen(word);
+		word = strtok(NULL, delimiters);
+	}
+	*pCount = count;
+	return wordsArray;
+}
+
+
+// general function to activate a function on each element of an array
+void generalArrayFunction(void* arr, int size, size_t typeSize, void* (*func)(void* element)) {
+	if (!arr) {
+		return;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		// use the function on each element of the array
+		func((char*)(arr)+i * typeSize);
+	}
+}
--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-
+#include <crtdbg.h> // TODO: remove before release
+#include "def.h"
 int main() {
-	printf("Hello, World! ACK SYN\n");
+
+
+
+	// Debug memory leaks
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_WNDW); // this is the pop up message (disable for memory leek location)
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
